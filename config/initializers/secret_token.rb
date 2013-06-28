f74fa9195c6a5ce1a80dafdfcd36d7338cc19aff
Andre M. Davis <andredavis5@hotmail.com>
2013-06-28 06:27:31 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuizzesOnRails::Application.config.secret_token = '2a47dc5507f39337cdefc5750a8d2d5542b0d26b90bf01930172c615a70b937dd241b4f5fd5b5f8bee66ef3c3cd07e1250f244aa057dc8fd75c54b22f31658e8'
